ac9e306cba1ec7193497479cff4721b1895b9592
wrang308 <gusmarwr@student.gu.se>
2017-12-21 22:09:38 +0100

    fixed notifications for the app

--- app/src/main/java/com/example/marku/thehundredlistapp/MainActivity.java
+++ app/src/main/java/com/example/marku/thehundredlistapp/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.marku.thehundredlistapp;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton settingsButton;
     TextView mainText;
 
-
+    public PendingIntent pendingIntent;
 
 
 
@@ -40,20 +49,109 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        //System.out.println(contentString);
+        //System.out.println("TIME" + time.getDays() + " Length " + mainTextArray.length);
+
+        mainText.setText(getDailyText());
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        int i = preferences.getInt("Numberoflaunches", 1);
+
+        // initialize the alarm
+        if (i < 2) {
+            i++;
+            alarmMethod();
+            editor.putInt("Numberoflaunches", i);
+            editor.commit();
+            Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_LONG).show();
+        }
+
+        //setAlarm();
+    }
+
+    public String getDailyText(){
+
+        String[] mainTextArray = getContentStringArr();
+
+        return mainTextArray[time.getDays()%mainTextArray.length];
+    }
+
+    public String[] getContentStringArr(){
         Cursor result = Mydb.getAllData();
         String contentString = "";
         while(result.moveToNext()){
             contentString = contentString  + result.getString(1) + "¤";
         }
         String[] mainTextArray = ArrayifyString(contentString);
-
-        time.getDays();
-        System.out.println(contentString);
-        System.out.println("TIME" + time.getDays() + " Length " + mainTextArray.length);
-
-        mainText.setText(mainTextArray[time.getDays() % mainTextArray.length]);
+        return mainTextArray;
     }
 
+
+    public  void alarmMethod(){
+
+
+
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 15);
+        calendar.set(Calendar.MINUTE, 30);
+        calendar.set(Calendar.SECOND, 0);
+        System.out.println(calendar.getTime());
+
+        Intent intent1 = new Intent(MainActivity.this, NotifyService.class);
+        intent1.putExtra("String", getContentStringArr());
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0,intent1, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager am = (AlarmManager) MainActivity.this.getSystemService(MainActivity.this.ALARM_SERVICE);
+        am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+//        Intent myIntent = new Intent(this , NotifyService.class);
+//        AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+//        pendingIntent = PendingIntent.getService(this, 0, myIntent, 0);
+//
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.set(Calendar.HOUR_OF_DAY, 12);
+//        calendar.set(Calendar.MINUTE, 00);
+//        calendar.set(Calendar.SECOND, 00);
+//
+//
+//        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 1000 * 120, pendingIntent);
+//
+//        System.out.println(calendar.getTimeInMillis());
+        //alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),  1000 * 60 * 60 * 24,
+          //      PendingIntent.getBroadcast(this, 2, myIntent,
+            //            PendingIntent.FLAG_UPDATE_CURRENT));
+
+
+    }
+
+    public void setAlarm() {
+        System.out.println("setAlarm Derek Banas");
+        // Define a time value of 5 seconds
+        //Long alertTime = new GregorianCalendar().getTimeInMillis()+5*1000;
+
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.HOUR, 0);
+        calendar.set(Calendar.AM_PM, Calendar.AM);
+
+        // Define our intention of executing AlertReceiver
+        Intent alertIntent = new Intent(this, NotifyService.class);
+
+        // Allows you to schedule for your application to do something at a later date
+        // even if it is in he background or isn't active
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        // set() schedules an alarm to trigger
+        // Trigger for alertIntent to fire in 5 seconds
+        // FLAG_UPDATE_CURRENT : Update the Intent if active
+        System.out.println(calendar.getTimeInMillis());
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),  1000 * 60 * 60 *24,
+                PendingIntent.getBroadcast(this, 1, alertIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT));
+
+    }
 
     public String[] ArrayifyString(String string){
         String[] array;
---
+++ app/src/main/java/com/example/marku/thehundredlistapp/NotifyService.java
@@ -0,0 +1,76 @@
+package com.example.marku.thehundredlistapp;
+
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+
+/**
+ * Created by andrew on 1/21/14.
+ */
+public class NotifyService extends BroadcastReceiver {
+
+    DataBaseHelper Mydb;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        Mydb = new DataBaseHelper(context);
+
+        Cursor result = Mydb.getAllData();
+        String contentString = "";
+        while(result.moveToNext()){
+            contentString = contentString  + result.getString(1) + "¤";
+        }
+        String[] mainTextArray = ArrayifyString(contentString);
+
+
+        String[] arr = intent.getStringArrayExtra("String");
+
+        //String[] arr = ArrayifyString(contentString);
+
+        System.out.println("Arr length " + arr.length);
+
+        int MID = 33;
+
+        long when = System.currentTimeMillis();
+        NotificationManager notificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Intent notificationIntent = new Intent(context, NotifyService.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+                notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        //Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder mNotifyBuilder = new NotificationCompat.Builder(
+                context).setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Go to the app to see your daily reminder")
+                .setContentText(mainTextArray[time.getDays() % mainTextArray.length])
+                .setAutoCancel(true).setWhen(when)
+                .setContentIntent(pendingIntent)
+                .setVibrate(new long[]{1000, 1000});
+        notificationManager.notify(MID, mNotifyBuilder.build());
+        MID++;
+
+    }
+
+    public String[] ArrayifyString(String string){
+        String[] array;
+        array = string.split("¤");
+        return array;
+    }
+
+}
